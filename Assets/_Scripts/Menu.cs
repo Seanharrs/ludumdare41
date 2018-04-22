e9aa80d699e43467ebb6d97c52f00b41a042c4af
KeshetBehanan <talisraeli225@gmail.com>
2018-04-22 15:40:14 +0300

    Game Over Menu

---
+++ Assets/_Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsMenu;
+
+    public void GoToScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void GoToScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void ActiveCredits()
+    {
+        if(creditsMenu == null)
+        {
+            return;
+        }
+        if(creditsMenu.activeSelf)
+        {
+            creditsMenu.SetActive(false);
+        }
+        else
+        {
+            creditsMenu.SetActive(true);
+        }
+    }
+}
