dbb7c97e0a71dfe1206c42a712fc375e69add8d1
KeshetBehanan <talisraeli225@gmail.com>
2018-04-23 00:56:25 +0300

    Bullets Update

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -5,12 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(PooledObject))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Bullet : MonoBehaviour
 {
     private float m_Damage;
     public float damage { get { return m_Damage; } }
 
     [SerializeField] private float m_velocity;
+
+    [SerializeField] private Sprite[] sprites;
+
+    private bool rotate;
+    private float rotateDirection;
+    private float rotateSpeed;
+
+    private void Start()
+    {
+        if(sprites != null)
+        {
+            var sprite = GetComponent<SpriteRenderer>();
+            sprite.sprite = sprites[Random.Range(0, sprites.Length)];
+            rotateDirection = Random.Range(0, 2) == 0 ? 1 : -1;
+            rotateSpeed = Random.Range(30f, 120f);
+        }
+    }
+
+    private void Update()
+    {
+        if(rotate)
+        {
+            transform.Rotate(rotateDirection * Vector3.forward * Time.deltaTime * rotateSpeed);
+        }
+    }
 
     public void SetDirection(Vector2 target)
     {
@@ -21,6 +47,8 @@ public class Bullet : MonoBehaviour
     public void Shoot()
     {
         GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(Vector2.right);
+        rotate = true;
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
         StartCoroutine(DeactiveDelay());
     }
 
