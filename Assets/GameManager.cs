82d655b30bb3bb645f79435868f11e8b21a8c2a2
George Garcia <garcia.illinois@gmail.com>
2018-04-22 19:13:19 -0400

    Connected scenes, moved camera and spawn points
    Connected Scenes (Main Menu to Map to Game Over Scene to Map), moved Camera and spawn points on Map, added credits to Game Over scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,15 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public string GameOverScene = "GameOver";
+
     [SerializeField]
     private int m_LivesLeft = 10;
-
+        
     public void LoseLife()
     {
         --m_LivesLeft;
         if(m_LivesLeft <= 0)
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene(GameOverScene);
         }
     }
 }
