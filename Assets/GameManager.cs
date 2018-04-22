0d4326df1f238f69d5ba102c7758e87c598986d4
Sean Harrison <seanharrs97@hotmail.com>
2018-04-22 20:55:54 +1000

    Add lose condition if too many enemies get past

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int m_LivesLeft = 10;
+
+    public void LoseLife()
+    {
+        --m_LivesLeft;
+        if(m_LivesLeft <= 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
